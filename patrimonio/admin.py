ef588d4448f05e72cc753e316c63aa3119b9b678
saulofmessias <121447124+saulofmessias@users.noreply.github.com>
2026-01-26 19:33:43 -0300

    Update admin.py

--- patrimonio/admin.py
+++ patrimonio/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import BemPatrimonial
 
 @admin.register(BemPatrimonial)
-class BemAdmin(admin.ModelAdmin):
-    list_display = ['numero_tombo', 'numero_nf', 'descricao', 'tipo', 'valor', 'secretaria', 'departamento', 'gerente_patrimonio']
-    list_filter = ['tipo', 'secretaria', 'departamento']
+class BemAdmin(ImportExportModelAdmin):  # ← ImportExport!
+    list_display = ['numero_tombo', 'descricao', 'tipo', 'valor', 'localizacao', 'secretaria', 'departamento']
+    list_filter = ['tipo', 'secretaria', 'departamento', 'localizacao']
     search_fields = ['numero_tombo', 'descricao']
-    list_editable = ['secretaria']
+    list_editable = ['departamento']
