9c6f9462c63c4d3ad710d213802adba43f3d9df1
saulofmessias <121447124+saulofmessias@users.noreply.github.com>
2026-01-28 18:30:18 -0300

    Update urls.py

--- gestao_patrimonio/urls.py
+++ gestao_patrimonio/urls.py
@@ -1,15 +1,2 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from patrimonio import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.dashboard, name='dashboard'),  # Página inicial
-    path('patrimonio/', include('patrimonio.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+from django.urls
