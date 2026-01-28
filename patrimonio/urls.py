7b7472efbb43f702186e43e2c1502f8e31a75c55
saulofmessias <121447124+saulofmessias@users.noreply.github.com>
2026-01-28 18:29:33 -0300

    Delete patrimonio/urls.py

--- patrimonio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.list_bens, name='list_bens'),
-    path('add/', views.add_bem, name='add_bem'),
-    path('detail/<str:pk>/', views.detail_bem, name='detail_bem'),
-]
