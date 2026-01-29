2b3d610af3f4db35f6948bd9a0e4664398ae364a
saulofmessias <saulofmessias@gmail.com>
2026-01-29 00:36:42 -0300

    admin

--- patrimonio/admin.py
+++ patrimonio/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from .models import *
+from .models import BemPatrimonial, CategoriaPatrimonio, Colaborador, Localizacao
 
 
 @admin.register(BemPatrimonial)
 class BemPatrimonialAdmin(admin.ModelAdmin):
-    list_display = ('numero_patrimonio', 'nome_patrimonio', 'status')
+    list_display = ['codigo', 'nome', 'categoria', 'localizacao']
+    list_filter = ['categoria', 'localizacao']
 
 
-admin.site.register(CategoriaPatrimonio)
-admin.site.register(Localizacao)
-admin.site.register(Colaborador)
+admin.site.register([CategoriaPatrimonio, Colaborador, Localizacao])
