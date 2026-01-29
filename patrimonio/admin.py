1aa4d1aed77aeb4b2f15164004e52b828838bb6b
saulofmessias <saulofmessias@gmail.com>
2026-01-28 22:35:45 -0300

    erro de importar

--- patrimonio/admin.py
+++ patrimonio/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-from .models import BemPatrimonial, CategoriaPatrimonio, Localizacao, Colaborador
+from .models import *
 
 
 @admin.register(BemPatrimonial)
 class BemPatrimonialAdmin(admin.ModelAdmin):
     list_display = ('numero_patrimonio', 'nome_patrimonio', 'status')
-    list_filter = ('status',)
-    search_fields = ('numero_patrimonio',)
 
 
 admin.site.register(CategoriaPatrimonio)
