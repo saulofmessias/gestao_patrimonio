c291509dfb4651ce1523372ab445353a0e7932f4
saulofmessias <121447124+saulofmessias@users.noreply.github.com>
2026-01-29 10:42:34 -0300

    Delete gestao_patrimonio/urls.py

--- gestao_patrimonio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from patrimonio import views  # ← Adicione esta linha
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.lista_patrimonio, name='lista_patrimonio'),  # ← Home lista
-    path('accounts/', include('django.contrib.auth.urls')),
-]
