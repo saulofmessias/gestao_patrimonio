638fee2656279e7786f9bf63b80d6066fd6f5b20
saulofmessias <saulofmessias@gmail.com>
2026-01-29 17:31:14 -0300

    Settings.py completo sem erros Render

--- gestao_patrimonio/settings.py
+++ gestao_patrimonio/settings.py
@@ -1,35 +1,115 @@
-import decouple  # Se usar .env
+import os
+from pathlib import Path
+from decouple import config  # pip install python-decouple
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-seu-secret-aqui-mude')
+
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+ALLOWED_HOSTS = ['*']  # Render
+
+INSTALLED_APPS = [
+    'jazzmin',  # Admin bonito
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'patrimonio',  # Seu app bens
+    'django_extensions',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Static Render
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'gestao_patrimonio.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'crispy_forms.context_processors.crispy_forms_template_pack',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'gestao_patrimonio.wsgi.application'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': decouple.config('DB_NAME', default='gestao_patrimonio'),
-        'USER': decouple.config('DB_USER', default='postgres'),
-        'PASSWORD': decouple.config('DB_PASSWORD', default=''),
-        'HOST': decouple.config('DB_HOST', default='localhost'),
-        'PORT': decouple.config('DB_PORT', default='5432'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', '5432'),
     }
 }
 
-# Cloudinary (adicione se não tiver)
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Belem'  # Pará
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Cloudinary
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': decouple.config('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': decouple.config('CLOUDINARY_API_KEY'),
-    'API_SECRET': decouple.config('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-# Login configs
+# Login
 LOGIN_URL = '/admin/'
 LOGIN_REDIRECT_URL = '/admin/'
 
-# Outros (INSTALLED_APPS já tem?)
-INSTALLED_APPS += [
-    'jazzmin',  # Se instalou
-    'cloudinary_storage',
-    'crispy_forms',
-    'crispy_bootstrap5',
-    'patrimonio',  # Seu app
-]
-CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
-CRISPY_TEMPLATE_PACK = 'bootstrap5'
+# Crispy
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# Jazzmin (Admin theme)
+JAZZMIN_SETTINGS = {
+    "site_title": "Gestão Patrimonial Capanema",
+    "site_header": "Prefeitura de Capanema",
+    "site_brand": "Patrimônio Público",
+    "copyright": "SM Informática",
+}
