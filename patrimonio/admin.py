ed44c0068a427b6945da402977697298fee894fe
saulofmessias <121447124+saulofmessias@users.noreply.github.com>
2026-01-29 11:53:46 -0300

    Update admin.py

--- patrimonio/admin.py
+++ patrimonio/admin.py
@@ -1,29 +1,25 @@
 from django.contrib import admin
 from .models import BemPatrimonial, CategoriaPatrimonio, Localizacao, Colaborador
 
-@admin.register(BemPatrimonial)
 class BemPatrimonialAdmin(admin.ModelAdmin):
-    list_display = ['numero_patrimonio', 'nome_patrimonio', 'status', 'categoria', 'localizacao']
+    # Formulário mais simples, campos opcionais
+    fields = ['numero_patrimonio', 'nome_patrimonio', 'imagem', 'quantidade', 'categoria', 
+              'custo_compra', 'data_compra', 'localizacao', 'colaborador_responsavel', 'status']
+    list_display = ['numero_patrimonio', 'nome_patrimonio', 'status']
     list_filter = ['status', 'categoria', 'criado_em']
     search_fields = ['nome_patrimonio', 'numero_patrimonio']
     list_editable = ['status']
-    list_per_page = 50
-    ordering = ['-criado_em']
 
 @admin.register(CategoriaPatrimonio)
 class CategoriaPatrimonioAdmin(admin.ModelAdmin):
-    list_display = ['id', 'nome', 'descricao']
-    search_fields = ['nome']
+    list_display = ['nome']
 
 @admin.register(Localizacao)
 class LocalizacaoAdmin(admin.ModelAdmin):
-    list_display = ['id', 'nome', 'cidade', 'gerente']
-    search_fields = ['nome', 'cidade']
-    list_filter = ['cidade']
+    list_display = ['nome', 'cidade']
 
 @admin.register(Colaborador)
 class ColaboradorAdmin(admin.ModelAdmin):
-    list_display = ['id', 'nome_completo', 'email', 'funcao', 'ativo']
-    search_fields = ['nome_completo', 'email']
-    list_filter = ['funcao', 'ativo']
-    list_editable = ['ativo']
+    list_display = ['nome_completo', 'email', 'ativo']
+
+admin.site.register(BemPatrimonial, BemPatrimonialAdmin)
