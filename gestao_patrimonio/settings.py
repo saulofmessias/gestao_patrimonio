09a61b7970b107aaf532d91621d1ace1e2431963
saulofmessias <saulofmessias@gmail.com>
2026-01-29 17:27:10 -0300

    Fix settings.py syntax + DATABASES Cloudinary

--- gestao_patrimonio/settings.py
+++ gestao_patrimonio/settings.py
@@ -1,77 +1,35 @@
-import os
-from pathlib import Path
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
-import dj_database_url
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-supersecreta123!@#')
-DEBUG = True  # ← TEMPORÁRIO pra ver erro!
-ALLOWED_HOSTS = ['capanema-patrimonio.onrender.com', 'localhost', '127.0.0.1']
-
-cloudinary.config(
-    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.environ.get('CLOUDINARY_API_KEY'),
-    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
-)
+import decouple  # Se usar .env
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': decouple.config('DB_NAME', default='gestao_patrimonio'),
+        'USER': decouple.config('DB_USER', default='postgres'),
+        'PASSWORD': decouple.config('DB_PASSWORD', default=''),
+        'HOST': decouple.config('DB_HOST', default='localhost'),
+        'PORT': decouple.config('DB_PORT', default='5432'),
+    }
 }
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'patrimonio',
-    'cloudinary',
+# Cloudinary (adicione se não tiver)
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': decouple.config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': decouple.config('CLOUDINARY_API_KEY'),
+    'API_SECRET': decouple.config('CLOUDINARY_API_SECRET'),
+}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# Login configs
+LOGIN_URL = '/admin/'
+LOGIN_REDIRECT_URL = '/admin/'
+
+# Outros (INSTALLED_APPS já tem?)
+INSTALLED_APPS += [
+    'jazzmin',  # Se instalou
     'cloudinary_storage',
-    'jazzmin',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'patrimonio',  # Seu app
 ]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'gestao_patrimonio.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'gestao_patrimonio.wsgi.application'
-
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth
-     
-LOGIN_URL = '/admin/login/'  # Ou '/login/' se quiser separado
-LOGIN_REDIRECT_URL = '/admin/'  # Após login, vai pro Admin
-LOGOUT_REDIRECT_URL = '/admin/login/'
-
-     
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
