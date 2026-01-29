38f00898357ea4323a06dab6479252a23de32ac0
saulofmessias <saulofmessias@gmail.com>
2026-01-29 17:36:06 -0300

    Fix model import views.py + urls patrimonio

--- gestao_patrimonio/urls.py
+++ gestao_patrimonio/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from patrimonio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.lista_patrimonio, name='lista_patrimonio'),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('patrimonio/', include('patrimonio.urls')),  # Seu app
 ]
--- patrimonio/views.py
+++ patrimonio/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from .models import BemPatrimonial
 
-def lista_patrimonio(request):
-    bens = BemPatrimonial.objects.all().order_by('-criado_em')
-    return render(request, 'patrimonio/list.html', {'bens': bens})
+
+def home(request):
+    return render(request, 'home.html')  # Depois criamos
