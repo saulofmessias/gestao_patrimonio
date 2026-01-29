b65bc034a304df4e29e16a3ba2b6816be1f9003b
saulofmessias <121447124+saulofmessias@users.noreply.github.com>
2026-01-29 10:25:20 -0300

    Update urls.py

--- gestao_patrimonio/urls.py
+++ gestao_patrimonio/urls.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-from patrimonio import views  # Adicione
+from django.urls import path
+from patrimonio import views  # ← Adicione esta linha
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.lista_patrimonio, name='lista_patrimonio'),  # Home
+    path('', views.lista_patrimonio, name='lista_patrimonio'),  # ← Home lista
     path('accounts/', include('django.contrib.auth.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
