15c6aeac08eddeffdefc61ff12a47d994e88f838
saulofmessias <saulofmessias@gmail.com>
2026-01-29 00:46:56 -0300

    add linhas

--- gestao_patrimonio/settings.py
+++ gestao_patrimonio/settings.py
@@ -3,10 +3,11 @@ from pathlib import Path
 import cloudinary  # NOVO: Para Cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import dj_database_url  # ← NOVO Render Postgres
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-supersecreta123!@#'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-supersecreta123!@#')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
@@ -18,6 +19,11 @@ cloudinary.config(
     api_key=os.environ.get('CLOUDINARY_API_KEY'),
     api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
 )
+
+# NOVO: DATABASE Render Postgres
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 INSTALLED_APPS = [
     'jazzmin',
@@ -62,38 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gestao_patrimonio.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
-]
-
-LANGUAGE_CODE = 'pt-br'
-TIME_ZONE = 'America/Sao_Paulo'
-USE_I18N = True
-USE_TZ = True
-
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
-# NOVO: Storage Cloudinary para fotos/PDFs
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-MEDIA_URL = '/media/'
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-JAZZMIN_SETTINGS = {
-    "site_title": "SM Patrimônio",
-    "site_header": "Capanema-PA",
-    "site_brand": "SIP",
-    "copyright": "Saulo Messias 2026"
-}
+    {'NAME': 'django.contrib.auth.password_validation
